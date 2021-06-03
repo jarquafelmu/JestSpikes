4a44e49f007175c3241468039af1fe29bd909a45
jarquafelmu <thechaoselement@gmail.com>
2021-06-02 19:02:35 -0600

    turns buffers to strings for comparison

--- Spike1/index.js
+++ Spike1/index.js
@@ -1,27 +1,30 @@
 //  compare file buffers
 const fs = require("fs");
 
+function normalize(str) {
+  // str = str.replace(/\s+$/, ""); // removes extra newline at end of file
+  str = str.replace(/\r?\n|\r/g, "\n"); // normalizes carriage returns to only be newlines
+  return str;
+}
+
 function main() {
   const args = process.argv.slice(2);
-  if (!args || args.length < 2) Error("expected two files for comparison");
+  if (!args || args.length != 2)
+    throw Error("expected two files for comparison");
 
-  console.log(args);
-
-  let fileA = fs.readFileSync(args[0]);
-  let fileB = fs.readFileSync(args[1]);
+  let fileA = fs.readFileSync(args[0]).toString();
+  let fileB = fs.readFileSync(args[1]).toString();
+  fileA = normalize(fileA);
+  fileB = normalize(fileB);
 
   // uses buffer equals for comparison
-  let result = fileA.equals(fileB);
-  console.log(`FileA equals FileB? ${result}`);
+  let result = fileA == fileB;
+  // let result = fileA,equals(fileB); // buffer compare
+  console.log(`Are files the same? ${result ? "Yes" : "No"}`);
 }
 
 try {
   main();
 } catch (e) {
-  console.error(`error: ${e}`);
+  console.error(`${e}`);
 }
-
-// "args": [
-//   "src.txt",
-//   "dst.txt"
-// ]
